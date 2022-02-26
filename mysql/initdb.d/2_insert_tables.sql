4a93c4d778ef6645582bb1db178f0cb25eeab39f
Syuto Onituka <onituka.0701@gmail.com>
2022-02-26 23:07:29 +0900

    booksテーブル追加

---
+++ mysql/initdb.d/2_insert_tables.sql
@@ -0,0 +1,11 @@
+USE sample_db;
+
+-- books table insert date
+INSERT INTO books
+    (id, title, author)
+VALUES
+    (1, "はだしのゲン", "長澤まさみ"),
+    (2, "人生", "野口英夫"),
+    (3, "お金に関する知識", "夏目漱石"),
+    (4, "動物図鑑", "マッカーサー"),
+    (5, "植物図鑑", "西野七瀬");
