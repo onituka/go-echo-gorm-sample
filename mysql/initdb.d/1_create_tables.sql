aa1f15f91f89ca91f1cdd8816fb683f8e2f711a5
Syuto Onituka <onituka.0701@gmail.com>
2022-02-27 17:46:17 +0900

    rentalbooksテーブルを追加

--- mysql/initdb.d/1_create_tables.sql
+++ mysql/initdb.d/1_create_tables.sql
@@ -12,9 +12,27 @@ CREATE TABLE books
 
 CREATE TABLE users
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
-    birthday DATE NOT NULL,
-    email VARCHAR(30) NOT NULL,
+    id       INT         NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(20) NOT NULL,
+    birthday DATE        NOT NULL,
+    email    VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE rental_books
+(
+    id              INT    NOT NULL AUTO_INCREMENT,
+    book_id         INT    NOT NULL,
+    user_id         INT    NOT NULL,
+    loan_date       DATE   NOT NULL,
+    return_date     DATE   DEFAULT NULL,
+    return_deadline DATE   NOT NULL,
+    completion      BIT(1) NOT NULL DEFAULT b'0',
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_rental_books_book_id (book_id)
+        REFERENCES books (id)
+        ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY fk_users_user_id (user_id)
+        REFERENCES users (id)
+        ON DELETE RESTRICT ON UPDATE RESTRICT
 );
