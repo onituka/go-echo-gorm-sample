bca029f7a640142de3d4d7a4894c0bacc9d2a72e
Syuto Onituka <onituka.0701@gmail.com>
2022-03-25 03:16:29 +0900

    rentalbookモデルにjsonタグを追加

--- domain/rentalbookdm/rental_book_model.go
+++ domain/rentalbookdm/rental_book_model.go
@@ -3,10 +3,10 @@ package rentalbookdm
 import "time"
 
 type RentalBook struct {
-	ID             int
-	BookID         int
-	UserID         int
-	LoanDate       time.Time
-	ReturnDate     time.Time
-	ReturnDeadline time.Time
+	ID             int       `json:"id"`
+	BookID         int       `json:"book_id"`
+	UserID         int       `json:"user_id"`
+	LoanDate       time.Time `json:"loan_date"`
+	ReturnDate     time.Time `json:"return_date"`
+	ReturnDeadline time.Time `json:"return_deadline"`
 }
