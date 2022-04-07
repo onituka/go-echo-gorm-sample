9d18f0cb48701178644c766f0ccd0a6365bb09cd
Syuto Onituka <onituka.0701@gmail.com>
2022-04-07 13:01:21 +0900

    bookモデルにnumberを追加

--- domain/bookdm/book_model.go
+++ domain/bookdm/book_model.go
@@ -6,6 +6,7 @@ type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
+	Number string `json:"number"`
 }
 
 func NewBook(title string, author string) (*Book, error) {
